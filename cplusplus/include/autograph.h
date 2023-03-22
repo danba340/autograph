dd552db620efeaa49bfb43536fbc6a524e80e218
Christoffer Carlsson <cc@christofferc.com>
2023-03-22 16:10:09 +0100

    Fix C++ header definitions (#5)

--- cplusplus/include/autograph.h
+++ cplusplus/include/autograph.h
@@ -1,12 +1,10 @@
 #pragma once
 
-namespace autograph {
+#include "autograph/types.h"
+
 constexpr unsigned int PRIVATE_KEY_SIZE = 32;
 constexpr unsigned int PUBLIC_KEY_SIZE = 32;
 constexpr unsigned int SIGNATURE_SIZE = 64;
-
-#include "autograph/types.h"
-#include "sodium.h"
 
 Party create_alice(const KeyPair &identity_key_pair);
 
@@ -16,7 +14,14 @@ Party create_initiator(const KeyPair &identity_key_pair);
 
 Party create_responder(const KeyPair &identity_key_pair);
 
+Party generate_alice();
+
+Party generate_bob();
+
+Party generate_initiator();
+
 KeyPair generate_key_pair();
 
+Party generate_responder();
+
 void init();
-}  // namespace autograph
--- cplusplus/src/autograph.cpp
+++ cplusplus/src/autograph.cpp
@@ -1,26 +1,48 @@
 #include "autograph.h"
 
+#include "autograph/create_key_pair.h"
 #include "autograph/create_party.h"
-#include "autograph/generate_ephemeral_key_pair.h"
+#include "sodium.h"
 
 Party create_alice(const KeyPair &identity_key_pair) {
-  Party alice = create_party(true, identity_key_pair);
+  auto alice = create_party(true, identity_key_pair);
   return std::move(alice);
 }
 
 Party create_bob(const KeyPair &identity_key_pair) {
-  Party bob = create_party(false, identity_key_pair);
+  auto bob = create_party(false, identity_key_pair);
   return std::move(bob);
 }
 
 Party create_initiator(const KeyPair &identity_key_pair) {
-  Party initiator = create_alice(identity_key_pair);
+  auto initiator = create_alice(identity_key_pair);
   return std::move(initiator);
 }
 
 Party create_responder(const KeyPair &identity_key_pair) {
-  Party responder = create_bob(identity_key_pair);
+  auto responder = create_bob(identity_key_pair);
   return std::move(responder);
+}
+
+Party generate_party(bool is_initiator) {
+  auto identity_key_pair = generate_key_pair();
+  auto party = create_party(is_initiator, identity_key_pair);
+  return std::move(party);
+}
+
+Party generate_alice() {
+  auto alice = generate_party(true);
+  return std::move(alice);
+}
+
+Party generate_bob() {
+  auto bob = generate_party(false);
+  return std::move(bob);
+}
+
+Party generate_initiator() {
+  auto initiator = generate_alice();
+  return std::move(initiator);
 }
 
 KeyPair generate_key_pair() {
@@ -31,6 +53,11 @@ KeyPair generate_key_pair() {
     throw std::runtime_error("Failed to generate Ed25519 key pair");
   }
   return std::move(key_pair);
+}
+
+Party generate_responder() {
+  auto responder = generate_bob();
+  return std::move(responder);
 }
 
 void init() {
